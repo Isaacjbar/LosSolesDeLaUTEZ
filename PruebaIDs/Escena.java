fd089fc92ca76aab80686597af8647211efd1c4d
IsaacJimenez <20233tn182@utez.edu,mx>
2024-05-30 23:30:56 -0600

    Version Mejor

--- PruebaIDs/Escena.java
+++ PruebaIDs/Escena.java
@@ -1,5 +1,44 @@
 package PruebaIDs;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Escena {
-    String [][] historiaA = new String[200][2];
+    String[][] historiaA = new String[200][2];
+    int currentSize = 0; // Para rastrear el número de escenas añadidas
+    String tituloHistoria; // Título de la historia
+
+    // Constructor para inicializar el título de la historia
+    public Escena(String titulo) {
+        this.tituloHistoria = titulo;
+    }
+
+    // Método para agregar una escena
+    public void agregarEscena(String id, String titulo) {
+        if (currentSize < historiaA.length) {
+            historiaA[currentSize][0] = id;
+            historiaA[currentSize][1] = titulo;
+            currentSize++;
+        } else {
+            System.out.println("No se pueden agregar más escenas, la matriz está llena.");
+        }
+    }
+
+    // Método para obtener una escena por ID
+    public String obtenerTituloPorID(String id) {
+        for (int i = 0; i < currentSize; i++) {
+            if (historiaA[i][0].equals(id)) {
+                return historiaA[i][1];
+            }
+        }
+        return "Escena no encontrada";
+    }
+
+    // Método para listar todas las escenas
+    public void listarEscenas() {
+        System.out.println("Título de la historia: " + tituloHistoria);
+        for (int i = 0; i < currentSize; i++) {
+            System.out.println("ID: " + historiaA[i][0] + " - Título: " + historiaA[i][1]);
+        }
+    }
 }
--- PruebaIDs/Main.java
+++ PruebaIDs/Main.java
@@ -3,47 +3,58 @@ package PruebaIDs;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-
-        ArrayList<String> Historia = new ArrayList<>();
+        ArrayList<Escena> historias = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        boolean crearHistoria = true, crearEscena = true;
-        int h=0,e=0;
+        boolean crearHistoria = true;
+        int h = 0;
 
-        //CREAR HISTORIAS
-        while(crearHistoria==true){
-
-            System.out.println("Titulo de la historia #"+(h+1));
-            Historia.add(sc.next());
-            
-            System.out.println("Titulo de historia #"+(h+1)+": "+ Historia.get(h));
+        // CREAR HISTORIAS
+        while (crearHistoria) {
+            System.out.println("Título de la historia #" + (h + 1));
+            String tituloHistoria = sc.next(); // Leer el título de la historia
+            Escena nuevaHistoria = new Escena(tituloHistoria); // Crear nueva historia con título
+            historias.add(nuevaHistoria);
             System.out.println("¿Deseas crear otra historia? \n Si(1) No(2)");
-            
-            if(sc.nextInt()==1){
-                crearHistoria=true;
+
+            if (sc.nextInt() == 1) {
+                crearHistoria = true;
             } else {
-                crearHistoria=false;
+                crearHistoria = false;
             }
             h++;
         }
-        crearHistoria=true;
 
-        //CREAR ESCENAS
-        while(crearEscena==true){
+        // CREAR ESCENAS
+        for (int i = 0; i < historias.size(); i++) {
+            Escena historiaActual = historias.get(i);
+            boolean crearEscena = true;
+            int e = 0;
 
-            System.out.println("Id de historia");
-            Historia.add(sc.next());
-            System.out.println("Titulo de historia #"+(e+1)+": "+ Historia.get(e));
-            System.out.println("¿Deseas crear otra historia? \n Si(1) No(2)");
-            
-            if(sc.nextInt()==1){
-                crearEscena=true;
-            } else {
-                crearEscena=false;
+            while (crearEscena) {
+                System.out.println("ID de la escena #" + (e + 1) + " de la historia #" + (i + 1));
+                String idEscena = sc.next();
+                System.out.println("Título de la escena #" + (e + 1) + " de la historia #" + (i + 1));
+                String tituloEscena = sc.next();
+                historiaActual.agregarEscena(idEscena, tituloEscena);
+                System.out.println("¿Deseas crear otra escena para esta historia? \n Si(1) No(2)");
+
+                if (sc.nextInt() == 1) {
+                    crearEscena = true;
+                } else {
+                    crearEscena = false;
+                }
+                e++;
             }
-            e++;
         }
+
+        // Listar todas las historias y sus escenas
+        for (int i = 0; i < historias.size(); i++) {
+            System.out.println("Historia #" + (i + 1));
+            historias.get(i).listarEscenas();
+        }
+
+        sc.close(); // Cerramos el Scanner al final del uso
     }
-    
 }
